39291e5e15a7c6d8670ba3558e624afe5a11a72f
Arturo Estrada <arturoestradanav@gmail.com>
2021-04-18 14:15:59 -0500

    Refactor spec files to use single quotes per linter complain

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,53 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-
-  subject {
+  subject do
     described_class.new(
-      title: "Eloquent Ruby",
-      description: "The ultimate Ruby guide",
-      author: "John",
-      price: "25.99",
+      title: 'Eloquent Ruby',
+      description: 'The ultimate Ruby guide',
+      author: 'John',
+      price: '25.99',
       merchant_id: 1,
       cart_id: 1
     )
-  }
+  end
 
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it "is not valid without a title" do
+  it 'is not valid without a title' do
     subject.title = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a description" do
+  it 'is not valid without a description' do
     subject.title = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a price" do
+  it 'is not valid without a price' do
     subject.price = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without an author" do
+  it 'is not valid without an author' do
     subject.author = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a merchant_id" do
+  it 'is not valid without a merchant_id' do
     subject.merchant_id = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without an cart_id" do
+  it 'is not valid without an cart_id' do
     subject.cart_id = nil
     expect(subject).to_not be_valid
   end
 
-  describe "Associations" do
+  describe 'Associations' do
     it { should belong_to(:cart) }
     it { should belong_to(:merchant) }
   end
