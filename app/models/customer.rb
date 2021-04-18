132cda56eb06e1cf92c456ac8b9ae58ef8ad7d83
Arturo Estrada <arturoestradanav@gmail.com>
2021-04-17 21:47:54 -0500

    Create test for the costumer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :address, :email, :name, :password, presence: true
+  # validates :email, :password, uniqueness: true
 end
---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  subject {
+    described_class.new(
+      address: "1428 Elm st",
+      email: "arturo@telos.com",
+      name: "Arturo",
+      password: "Loremipsum"
+    )
+  }
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without an address" do
+    subject.address = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+end
