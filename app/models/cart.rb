1919ebe3548281897b9d223b5a3dd78a142a8761
Arturo Estrada <arturoestradanav@gmail.com>
2021-04-20 10:11:46 -0500

    Set cart addition and removal behavior

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,44 @@
 class Cart < ApplicationRecord
   belongs_to :customer
   has_many :books
+
+  def add_book(book_price)
+    add_to_subtotal(book_price)
+    add_to_service_fee(1.0)
+    set_total
+    self.save
+  end
+
+  def remove_book(book_price)
+    remove_from_subtotal(book_price)
+    remove_from_service_fee(1.0)
+    set_total
+    self.save
+  end
+
+  private
+
+  def set_total
+    self.total = (self.subtotal + self.service_fee).round(2)
+  end
+
+  def add_to_subtotal(amount)
+    self.subtotal += amount
+    self.subtotal = self.subtotal.round(2)
+  end
+
+  def remove_from_subtotal(amount)
+    self.subtotal -= amount
+    self.subtotal = self.subtotal.round(2)
+  end
+
+  def add_to_service_fee(amount)
+    self.service_fee += amount
+    self.service_fee = self.service_fee.round(2)
+  end
+
+  def remove_from_service_fee(amount)
+    self.service_fee -= amount
+    self.service_fee = self.service_fee.round(2)
+  end
 end
