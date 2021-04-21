0cd4250ee882e4cf064eb6f36dddbed427b9b091
Arturo Estrada <arturoestradanav@gmail.com>
2021-04-21 17:42:15 -0500

    Create custom routes for merchant devise

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,14 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
   patch 'credit', to: 'customers#request_store_credit'
-  devise_for :customers
-  devise_for :merchants
+  devise_for :customers, path: 'customers'
+  devise_for :merchants, path: 'merchants', controllers: {
+    sessions: "merchants/sessions",
+    passwords: "merchants/passwords",
+    registrations: "merchants/registrations",
+    confirmations: "merchants/confirmations",
+    unlocks: "merchants/unlocks"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
